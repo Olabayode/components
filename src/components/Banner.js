13603e77ac23670292b865f08afb52f6cc2d92c1
Emmanuel Olure <emmanuelolabayode@gmail.com>
2026-06-03 18:46:28 -0500

    styled the header

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,14 +1,14 @@
 import Button from "./Button";
 function Banner (props){
     return (
-        <div>
+        <section className="banner">
             <h1>{props.bannerHeader}</h1>
             <h2>{props.bannerText}</h2>
-            <div>
+            <div className="banner-actions">
                 <Button className={props.buttonText[0].className} buttonText={props.buttonText[0].text}/>
                 <Button className={props.buttonText[1].className} buttonText={props.buttonText[1].text}/>
             </div>
-        </div>
+        </section>
     );
 
 }
